46d43aca1351b77db4c671fdc2911eac55ab7519
Yakshit Chawla <chawla.yakshit@gmail.com>
2022-06-08 13:05:58 -0400

    setup routing tabs

---
+++ src/layout/layout.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import About from '../components/about'
+import Home from '../components/home'
+import Contact from '../components/contact'
+import Projects from '../components/projects'
+import Nav from '../navigation/nav'
+
+
+function Layout() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Nav />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="projects" element={<Projects />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Layout
---
+++ src/navigation/nav.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
+const nav = () => {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/projects">Projects</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet></Outlet>
+        </>
+    )
+}
+
+export default nav
