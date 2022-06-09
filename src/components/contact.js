69bc10df60d1f8d96c3a2a16ba95bfef52569f58
Yakshit Chawla <chawla.yakshit@gmail.com>
2022-06-09 18:45:47 -0400

    contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const contact = () => {
   return (
-    <div className="name">Contact</div>
+    <div className="name">
+      <h1>Are you interested in hiring me?</h1>
+    </div>
   )
 }
 
